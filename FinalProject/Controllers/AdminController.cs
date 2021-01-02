b3be4794348b4097c8a6ef8b6a1c2f0f2ba4735f
sabbir2joy <sabbir2joy@gmail.com>
2021-01-02 16:26:39 +0600

    AdminController added

---
+++ FinalProject/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using FinalProject.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinalProject.Controllers
+{
+    public class AdminController : ApiController
+    {
+        OnlineStudentsRepository stdRepo = new OnlineStudentsRepository();
+
+
+        [Route("api/Admins/Students")]
+
+        //Get All Students Information
+        public IHttpActionResult Get()
+        {
+
+            return Ok(stdRepo.GetAll());
+        }
+    }
+}
