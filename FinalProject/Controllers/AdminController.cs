3dcc4c32481baa81d6e426cf1ea55acb9daa6ace
sabbir2joy <sabbir2joy@gmail.com>
2021-01-02 16:36:49 +0600

    Get student info at AdminController class

--- FinalProject/Controllers/AdminController.cs
+++ FinalProject/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using FinalProject.Repository;
+﻿using FinalProject.Models;
+using FinalProject.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,5 +22,20 @@ namespace FinalProject.Controllers
 
             return Ok(stdRepo.GetAll());
         }
+        //Get a Specific Student Information
+        [Route("api/Admins/Students/{id}", Name = "GetStudentById")]
+        public IHttpActionResult Get(int id)
+        {
+            OnlineStudent onlineStudent = stdRepo.GetById(id);
+
+
+            if (onlineStudent == null)
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+
+            return Ok(onlineStudent);
+        }
+
     }
 }
